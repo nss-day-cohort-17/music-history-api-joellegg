702503c5c5206b745edebe3fed0290bcd440b72e
joellegg <joellegg@bellsouth.net>
2017-04-19 14:26:47 -0500

    successfully adding new song

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -7,22 +7,28 @@ const Songly = require('../models/song')
 // < use model methods for getting all songs and one song then send the response back with the data>
 module.exports.getSongs = (req, res, next) => {
   Songly.getAll()
-  .then((songs) => {
-    res.status(200).json(songs)
-  })
-  .catch((err) => {
-    next(err)
-  })
+  .then(songs => res.status(200).json(songs))
+  .catch(err => next(err))
 }
 
 module.exports.getSong = ({params: {id}}, res, next) => {
   Songly.getSingle(id)
-  .then((song) => {
-    res.status(200).json(song)
-  })
-  .catch((err) => {
-    next(err)
-  })
+  .then(song => res.status(200).json(song))
+  .catch(err => next(err))
 }
 
 // <stretch goal: methods for adding, deleting, editing a song>
+// curl --request POST --data "{ \"SongId\": 24, \"Title\": \"Tongue Tied\", \"SongLength\": 425, \"ReleaseDate\": \"8/5/2014\", \"GenreId\": 13, \"ArtistId\": 7, \"AlbumId\": 23 }" http://127.0.0.1:3000/api/v1/songs/new --header "Content-Type: application/json"
+module.exports.addSong = ({body}, res, next) => {
+  Songly.forge(body)
+  .save()
+  .then(() => res.status(201).json({'msg': 'Song added successfully'}))
+  .catch(err => next(err))
+}
+
+module.exports.deleteSong = ({params: {id}}, res, next) => {
+  Songly.forge({id})
+  .destroy()
+  .then(song => res.status(202).json(song))
+  .catch(err => next(err))
+}
--- routes/songs.js
+++ routes/songs.js
@@ -5,12 +5,13 @@ const { Router } = require('express')
 const router = Router()
 
 // <require the songCtrl to get access to its methods>
-const { getSong, getSongs } = require('../controllers/songCtrl')
+const { getSong, getSongs, addSong } = require('../controllers/songCtrl')
 
 
 // <define routes for getting all songs and a single song>
 // <stretch: define routes for posting, deleting, editing a song>
 router.get('/songs', getSongs)
 router.get('/songs/:id', getSong)
+router.post('/songs/new', addSong)
 
 module.exports = router
