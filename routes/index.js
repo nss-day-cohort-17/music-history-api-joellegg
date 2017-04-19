2041b16c669d1fc0a6aadb9c2ad49fe6b8c514d0
joellegg <joellegg@bellsouth.net>
2017-04-19 10:04:33 -0500

    set up route files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 // <require express Router and require the songs.js router file>
-
+const { Router } = require('express')
+const router = Router()
 
 // this is an example of a root document. It returns a json representation of
 // all of our endpoints. (Just one endpoint exists in this tiny example app)
@@ -11,3 +12,5 @@ router.get('/', function(req, res) {
     "songs": "http://localhost:3000/api/v1/songs",
   });
 });
+
+module.exports = router
--- routes/songs.js
+++ routes/songs.js
@@ -1,7 +1,16 @@
 'use strict';
 
 // <require the express Router>
+const { Router } = require('express')
+const router = Router()
+
 // <require the songCtrl to get access to its methods>
+const { } = require('../controllers/songCtrl')
+
 
 // <define routes for getting all songs and a single song>
 // <stretch: define routes for posting, deleting, editing a song>
+router.get('/songs', getSongs)
+router.get('/songs/:id', getSong)
+
+module.exports = router
