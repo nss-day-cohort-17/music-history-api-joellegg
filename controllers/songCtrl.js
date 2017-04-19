0503d00e6fa90e642c29c73d49fd1dfcfd34f41d
joellegg <joellegg@bellsouth.net>
2017-04-19 10:11:24 -0500

    set up controller to get song and songs

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -1,5 +1,28 @@
 'use strict'
 
 // <require a song model>
+const { bookshelf } = require('../musichistory')
+const Song = require('../models/song')
+
 // < use model methods for getting all songs and one song then send the response back with the data>
+module.exports.getSongs = (req, res, next) => {
+  Song.getAll()
+  .then((songs) => {
+    res.status(200).json(songs)
+  })
+  .catch((err) => {
+    next(err)
+  })
+}
+
+module.exports.getSong = ({params: {id}}, res, next) => {
+  Song.getSingle(id)
+  .then((song) => {
+    res.status(200).jsong(song)
+  })
+  .catch((err) => {
+    next(err)
+  })
+}
+
 // <stretch goal: methods for adding, deleting, editing a song>
